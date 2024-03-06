822fe186826b06db7ac2bd9c3dc1e1b251566247
hendesprit <hend.zormati@esprit.tn>
2024-03-06 16:31:04 +0100

    sort + dynamic search publication  + only show the categorie that contains at least oone subcategory

---
+++ src/Form/EditPublicationType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Publication;
+use App\Entity\SousCategorie;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Doctrine\ORM\EntityRepository;
+
+
+class EditPublicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            // ->add('User', EntityType::class, [
+            //     'class' => User::class,
+            //     'choice_label' => 'id',
+            //     ])
+            // ->add('Categorie', EntityType::class, [
+            //     'class' => Categorie::class,
+            //     'choice_label' => 'nom',
+            //      ])
+            // ->add('SousCategorie', EntityType::class, [
+            //     'class' => SousCategorie::class,
+            //     'choice_label' => 'nom',
+            //     ])
+            ->add('titre')
+            ->add('contenu')
+            ->add('comOuvert')
+            ->add('anonyme')
+            ->add('imageFile', FileType::class, [
+                'label' => 'Publication Picture',
+                'mapped' => false, // This field is not mapped to any entity property
+                'required' => false,
+            ])
+            ;
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Publication::class,
+        ]);
+    }
+}
